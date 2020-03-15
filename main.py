74620dd4f4f4b7e7e0c8290eb7cc2f5e2bb1cd65
abdo-soliman <abdelrahman.soliman97@eng-st.cu.edu.eg>
2020-03-15 05:56:48 +0200

    made backgrond segmentation optional

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from deeplabv3 import DeepLabModel, label_to_color_image
 
 TEMPLATE_IMAGE_PATH = os.path.join(os.getcwd(), "models", "template.jpg")
 MODEL_PATH = os.path.join(os.getcwd(), "models", "deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz")
+SEGMENT_BACKGROUND = False
 GAMAL_X_START = 85
 GAMAL_X_END = 255
 GAMAL_Y_START = 80
@@ -51,10 +52,13 @@ def get_faces(img):
 def gaficate(input_file, output_dir):
     template = cv2.imread(TEMPLATE_IMAGE_PATH)
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    pil_input_img = Image.open(input_file)
     input_img = cv2.imread(input_file)
 
-    segmented_image = segmentation(pil_input_img, input_img)
+    if SEGMENT_BACKGROUND:
+        pil_input_img = Image.open(input_file)
+        segmented_image = segmentation(pil_input_img, input_img)
+    else:
+        segmented_image = input_img
 
     count = 1
     for face in get_faces(segmented_image):
@@ -73,12 +77,14 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('-i', '--input', default="input.jpg", type=str, help='input image to be gaficated')
     parser.add_argument('-o', '--output_dir', default="output", type=str, help='gafication output filename')
+    parser.add_argument('-es', '--enable_segmentation', default=False, type=bool, help='enable background segmentation')
     parser.add_argument('-px', '--padding_x', default=PADDING_X, type=str, help='face width padding in pixels')
     parser.add_argument('-py', '--padding_y', default=PADDING_Y, type=str, help='face height padding in pixels')
 
     args = parser.parse_args()
     input_file = args.input
     output_dir = args.output_dir
+    SEGMENT_BACKGROUND = args.enable_segmentation
     PADDING_X = args.padding_x
     PADDING_Y = args.padding_y
 
